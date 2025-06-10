a655d4779526dc9fd6445e58a12fe95cdd98cb5b
vishuna2010 <vparague@gmail.com>
2025-06-10 17:01:31 -0400

    fonts resolution

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  debug: true,
   devServer: {
     port: 3001
   },
@@ -25,7 +26,7 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
         {
           href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap',
           rel: 'stylesheet'
